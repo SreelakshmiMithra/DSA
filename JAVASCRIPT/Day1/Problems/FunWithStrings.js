beae6f13ec3f664798605701f77047726c939af0
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2026-01-02 10:32:45 +0530

    Add basic JavaScript string concatenation example

---
+++ JAVASCRIPT/Day1/Problems/FunWithStrings.js
@@ -0,0 +1,7 @@
+let first_Name = "letStart"
+let last_Name = "Coding"
+let full_Name = first_Name + last_Name;
+let full_Name1 = first_Name + " " + last_Name;//// Space Added using emptystring
+console.log(full_Name);
+console.log(full_Name1);
+
