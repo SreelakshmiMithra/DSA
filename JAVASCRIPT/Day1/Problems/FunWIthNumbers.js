b915620d3edf4be449ef78a437005d37f4c82709
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2026-01-02 10:21:32 +0530

    Basic operations on Numbers

---
+++ JAVASCRIPT/Day1/Problems/FunWIthNumbers.js
@@ -0,0 +1,21 @@
+// Perform basic operations and give the result on the console
+let a = 10;//declaration+initialization
+let b = 2;
+let N = 3;
+a = 6;//Reassigning  a value the last given value to a will be considered
+let sum = a + b;
+let difference = a - b;
+let multiply = a * b;
+let divide = a / b;
+let sq = a * a;
+let a_raised_to_power_N = a ** N;
+console.log(sum);
+console.log(difference);
+console.log(multiply);
+console.log(divide);
+console.log(sq);
+console.log(a_raised_to_power_N);
+
+
+
+
