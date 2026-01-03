41b0d947bea14ed8a03a8622ba8e4e555b2dd8e1
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2026-01-03 09:19:43 +0530

    Add functions and call them to display output

---
+++ JAVASCRIPT/Day2/Problems/sampleFunction.js
@@ -0,0 +1,12 @@
+function printHelloWorld(){
+    console.log("Hello,world");
+}
+printHelloWorld();
+
+function print(){
+    let a=10;
+    console.log(a);
+}
+print();
+//A function won’t run by itself; you must call it.
+//Only when called, the console.log inside will show the output.
