e0b20a7b14c66cc46fbee6c14f3ddc1de74ce9e7
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2026-01-03 07:02:46 +0530

    I mplemented sum function with sample input

---
+++ JAVASCRIPT/Day1/Practice/sumofTwoNumbers.js
@@ -0,0 +1,24 @@
+//Problem Statement:
+//Given two integers a and b, compute and return their sum.
+
+//Input Format:
+//Two integers are provided as input, each on a new line.
+
+//Sample Input:
+//2
+//3
+
+//Sample Output:
+//5
+
+// Function to compute the sum of two integers
+function  sum(a,b){
+ return a+b;
+}
+//testcases:
+let a=2;
+let b=3;
+console.log(sum(a,b));
+
+
+
