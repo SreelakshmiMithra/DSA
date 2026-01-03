ef4ff533de20b9bb5e4f04bc8c7d0f3f444537e6
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2026-01-03 11:33:00 +0530

    Add square function using return statement

---
+++ JAVASCRIPT/Day2/Problems/squareFunction.js
@@ -0,0 +1,14 @@
+// Function to calculate the square of a number
+function square(x) {
+    // Multiply the number by itself
+    let result = x * x;
+
+    // send the result back
+    return result;
+}
+
+// Call the function with input value 5
+let value = square(5);
+
+// Print the result to the console
+console.log("Square of 5 is:", value);
