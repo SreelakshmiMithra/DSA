baf39c9f6016b2dd9e69698d2026a5f1a4e5ceaa
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2026-01-04 12:27:17 +0530

    Implemented array iteration to print elements, length, and even numbers

---
+++ JAVASCRIPT/DAY3/Problems/iterateUserArray.js
@@ -0,0 +1,26 @@
+let arr=[10,9,8,7,6,5,4,3,2,1];
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+// console.log(arr[0]);
+ let length=arr.length;
+ console.log("Length of the array is:", length)
+
+
+ //print all even numbers in the array 
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+        console.log(arr[i])
+
+    }
+  }
+   
+
+
+
+
+
+
+
+
