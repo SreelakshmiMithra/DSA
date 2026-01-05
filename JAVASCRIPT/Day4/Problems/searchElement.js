e69c46a165120d2af838acdaa56b55079e1263cd
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2026-01-05 11:28:48 +0530

    Add linear search function returning index or -1

--- JAVASCRIPT/Day4/Problems/searchElement.js
+++ JAVASCRIPT/Day4/Problems/searchElement.js
@@ -20,3 +20,20 @@ if (result === -1) {
   console.log("Element found, index is:", result);
 }
 
+
+//  Parameter-based approach 
+
+function searchElement1(arr, x) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === x) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+let arr1 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let arr2 = [4, 2, 0, 10, 8, 30];
+
+console.log(searchElement1(arr2, 10)); // 3
+console.log(searchElement1(arr2, 45)); // -1
